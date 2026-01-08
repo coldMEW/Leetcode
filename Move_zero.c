76027d975418d26d7dc0094ef60b2d7a39b044ef
coldMEW <shah.isbibek@gmail.com>
2026-01-07 20:48:38 -0600

    Actual solution for leetcode problem 283:Moving Zero

--- Move_zero.c
+++ Move_zero.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 void moveZeroes(int* nums, int numsSize) {
-    int last = numsSize -1; 
-    int count = 0;
-    for (int x = 0; x < numsSize - count; x++)
+    int position = 0;
+    for (int x = 0; x < numsSize; x++)
     {
-        if (nums[x] == 0 && x != last)
+        if (nums[x] != 0)
         {
-            int temp = nums[last];
-            nums[last] = nums[x];
+            int temp = nums[position];
+            nums[position] = nums[x];
             nums[x] = temp;
-            last = last - 1;
-            count = count + 1;
+            position = position + 1;
         }
 
     }
-
 }
+
+
+
 
 int main()
 {
@@ -27,4 +27,26 @@ for (int x = 0; x < 5; x++)
     printf("%d ", nums[x]);
 }
 return 0;
+}
+
+
+//Just move zero
+void moveZeroes1(int* nums, int numsSize) {
+    int last = numsSize -1; 
+    int count = 0;
+    for (int x = 0; x < numsSize - count; x++)
+    {
+        if (nums[x] == 0)
+        {
+            int temp = nums[last];
+            nums[last] = nums[x];
+            nums[x] = temp;
+            last = last - 1;
+            count = count + 1;
+        }
+        
+
+        
+    }
+
 }
